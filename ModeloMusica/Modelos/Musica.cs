e10fd0c3fa43aa18b37ee5cb8ab8c37c99c0a61e
Lucas Lira <lucas_afl1505@hotmail.com>
2023-11-05 15:18:07 -0300

    Adicionar arquivos de projeto.

---
+++ ModeloMusica/Modelos/Musica.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ModeloMusica.Modelos
+{
+    internal class Musica
+    {
+        [JsonPropertyName("song")]
+        public string? rMusica { get; set; }
+
+        [JsonPropertyName("artist")]
+        public string? rArtista { get; set; }
+
+        [JsonPropertyName("duration_ms")]
+        public float dDuracao { get; set; }
+
+        [JsonPropertyName("genre")]
+        public string? rGene { get; set; }
+
+        public void ExibirDetalhesMusica(int _contador)
+        {
+            using(HttpClient httpClient = new HttpClient())
+            {                
+                Console.WriteLine(_contador + " - Artista: " + rArtista + " / Música: " + rMusica +
+                    " / Duração de: " + dDuracao + " / Genero: " + rGene);
+            }
+        }
+
+        public void ExibirGeneros(int _contador)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                Console.WriteLine(_contador + " - Genero: " + rGene);
+            }
+        }
+    }
+}
---
+++ ModeloMusica/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+using ModeloMusica.Modelos;
+
+try
+{
+    using (HttpClient client = new HttpClient())
+    {
+
+        //1. EXIBIR TODOS AS INFORMAÇÕES
+        using (HttpClient httpclient = new HttpClient())
+        {
+            string resposta = await httpclient.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+            var lstMusica = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+            for (int i = 0; i < lstMusica.Count; i++)
+            {
+                lstMusica[i].ExibirDetalhesMusica(i);
+            }
+        }
+        //#######################################################################
+
+        //2. EXIBIR TODOS OS GÊNEROS MUSICAIS DA LISTA;
+        //using (HttpClient httpclient = new HttpClient())
+        //{
+        //    string resposta = await httpclient.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        //    var lstMusica = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        //    for (int i = 0; i < lstMusica.Count; i++)
+        //    {
+        //        lstMusica[i].ExibirGeneros(i);
+        //    }
+        //}
+        //#######################################################################
+
+        //3. ORDERRNAR OS ARTISTAS POR NOME
+        //using (HttpClient httpclient = new HttpClient())
+        //{
+        //    string resposta = await httpclient.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        //    List<Musica> lstMusica = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        //    List<string> lstArtistaOrdenado = new List<string>();
+        //    for (int i = 0; i < lstMusica.Count; i++)
+        //    {
+        //        lstArtistaOrdenado.Add(lstMusica[i].rArtista);
+        //    }
+        //   lstArtistaOrdenado.Sort();
+        //}
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Erro em: " + ex.Message);
+}
