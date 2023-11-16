865fa8a9210885e9e16252bb77ff8dd85efef4c4
Lucas Lira <lucas_afl1505@hotmail.com>
2023-11-15 21:30:31 -0300

    New: Adding new method GeraArquivoJson() - Função que gera um arquivo com todas as músicas adicionadas como favorita

---
+++ ModeloMusica/Modelos/MusicasPreferidas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ModeloMusica.Modelos
+{
+    internal class MusicasPreferidas
+    {
+        public string? rNome { get; set; }
+        public List<Musica> lstMusicasFavoritas { get; set; }
+
+        public MusicasPreferidas(string _nome)
+        {
+
+            rNome = _nome;
+            lstMusicasFavoritas = new List<Musica>();
+        }
+
+        public void addMusicasFavoritas(Musica _musica)
+        {
+            lstMusicasFavoritas.Add(_musica);
+        }
+
+        public void ExibirMusicasFavoritas()
+        {
+            Console.WriteLine("Essas são as músicas favoritas -> " + rNome);
+            foreach (var musica in lstMusicasFavoritas)
+            {
+                Console.WriteLine(musica.rArtista + " - " + musica.rMusica);
+            }
+        }
+
+        public void GeraArquivoJson()
+        {
+            try
+            {
+
+                string json = JsonSerializer.Serialize(new
+                {
+                    nome = rNome,
+                    musicas = lstMusicasFavoritas
+                });
+
+                string nomeArquivo = $"Musicas_Favoritas_{rNome}.json";
+                File.WriteAllText(nomeArquivo, json);
+                string pathFile = Path.GetFullPath(nomeArquivo);
+                Console.WriteLine("Arquivo salvo com sucesso no caminho: " + pathFile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro em MusicasPreferidas - GeraArquivoJson() " + ex.Message);
+            }
+
+        }
+    }
+}
--- ModeloMusica/Program.cs
+++ ModeloMusica/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Json;
 using ModeloMusica.Modelos;
 using ModeloMusica.Filtros;
+using System.Security.Cryptography.X509Certificates;
 
 try
 {
@@ -8,34 +9,65 @@ try
     {
         using (HttpClient httpclient = new HttpClient())
         {
-            string resposta = await httpclient.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+            string resposta;
+            string strArtistaEscolhido;
+            string strEstiloMusicalEscolhido;
+
+            resposta = await httpclient.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
 
             var lstMusica = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
 
-            //EXIBIR DETALHES DE TODAS AS MUSICAS
-            for (int i = 0; i < lstMusica.Count; i++)
-            {
-                lstMusica[i].DetalhesMusicas(i);
-            }
+            //1. EXIBIR DETALHES DE TODAS AS MUSICAS
+            //for (int i = 0; i < lstMusica.Count; i++)
+            //{
+            //    lstMusica[i].DetalhesMusicas(i);
+            //}
 
-            //GENEROS DISTINTOS
-            LinqFilter.DistinctGeneros(lstMusica);
+            //2. GENEROS DISTINTOS
+            //LinqFilter.DistinctGeneros(lstMusica);
 
-            //ARTISTAS DISTINTOS
-            LinqFilter.DistinctArtistas(lstMusica);
+            //3. ARTISTAS DISTINTOS
+            //LinqFilter.DistinctArtistas(lstMusica);
 
-            //ORDENAR E EXIBIR ARTISTAS
-            LinqOrder.ExibirArtistasOrdenados(lstMusica);
+            //4. ORDENAR E EXIBIR ARTISTAS
+            //LinqOrder.ExibirArtistasOrdenados(lstMusica);
 
-            //FILTRAR ARTISTA DE ACORDO COM O ESTILO MUSICAL ESCOLHIDO
-            LinqFilter.FiltrarArtistaConformeGenero(lstMusica, "pop");
+            //5. FILTRAR ARTISTA DE ACORDO COM O ESTILO MUSICAL ESCOLHIDO
+            //LinqFilter.DistinctGeneros(lstMusica);
+            //MensagemConsole();
+            //strEstiloMusicalEscolhido = Console.ReadLine()!;
+            //LinqFilter.FiltrarArtistaConformeGenero(lstMusica, strEstiloMusicalEscolhido);
 
-            //FILTRAR MUSICAS DE ACORDO COM A MUSICA ESCOLHIDA
-            LinqFilter.FiltrarMusicasConformeArtista(lstMusica, "Post Malone");
+            //6. FILTRAR MUSICAS DE ACORDO COM A MUSICA ESCOLHIDA
+            //LinqFilter.DistinctArtistas(lstMusica);
+            //MensagemConsole();
+            //strArtistaEscolhido = Console.ReadLine()!;
+            //LinqFilter.FiltrarMusicasConformeArtista(lstMusica, strArtistaEscolhido);
+
+            //7. MUSICAS PREFERIDAS
+            MusicasPreferidas musicasPreferidas;
+            musicasPreferidas = new MusicasPreferidas("Lira");
+
+            musicasPreferidas.addMusicasFavoritas(lstMusica[2]);
+            musicasPreferidas.addMusicasFavoritas(lstMusica[71]);
+            musicasPreferidas.addMusicasFavoritas(lstMusica[139]);
+            musicasPreferidas.addMusicasFavoritas(lstMusica[583]);
+            musicasPreferidas.addMusicasFavoritas(lstMusica[938]);
+
+            musicasPreferidas.ExibirMusicasFavoritas();
+
+            musicasPreferidas.GeraArquivoJson();
+
         }
     }
 }
 catch (Exception ex)
 {
     Console.WriteLine("Erro em: " + ex.Message);
+}
+
+static void MensagemConsole()
+{
+    Console.WriteLine("################################################");
+    Console.WriteLine("Escolha um das opções mencionadas acima:");
 }
